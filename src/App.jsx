e6df882bd94c9936927621796c2fdf4cddfc6171
Kaveesha Dilshan <kaveeshadilshankd23@gmail.com>
2025-07-09 08:49:45 +0530

    Clean up the bolierplate, add the assets and set up the dependencies

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const App = () => {
+  return (
+    <main>
+        
+    </main>
+  )
+}
+
+export default App
