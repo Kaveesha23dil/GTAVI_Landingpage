d78d5272c020b2a05d3d86d3ae9b08265c4fed33
Kaveesha Dilshan <kaveeshadilshankd23@gmail.com>
2025-07-09 23:59:36 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+import React from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
 
-gsap.registerPlugin(ScrollTrigger)
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
-        
+      <NavBar />
+      <Hero />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
