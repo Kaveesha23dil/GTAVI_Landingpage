a200391482d8d2eb54c0f09e41cafae5ede7247f
Kaveesha Dilshan <kaveeshadilshankd23@gmail.com>
2025-07-18 01:40:04 +0530

    Implement Lucia

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
+import Lucia from "./sections/Lucia";
 
 
 gsap.registerPlugin(ScrollTrigger);
@@ -19,6 +20,7 @@ const App = () => {
       <FirstVideo />
       <Jason />
       <SecondVideo/>
+      <Lucia />
     </main>
   );
 };
